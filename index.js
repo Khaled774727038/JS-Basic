87775b05c258e8156bba2b6a2de2c50085b5524e
[Khaled Al-Hammadi] <[khaled Al-Hammadi]>
2024-02-06 21:42:29 +0100

    my JS-Basic

---
+++ index.js
@@ -0,0 +1,68 @@
+//1. Print 1 - 135
+/*
+for (let i = 1; i <= 135; i++){
+    console.log(i);
+}
+*/
+/*2. Print Odd Numbers 1 - 135
+let oddNumbers = 'i';
+for(i=1; i<=135; i+=2){
+ console.log(i);
+};
+*/
+/*3. Sum of Printed Numbers
+let sum = 0;
+
+for (let i = 1; i <= 135; i++) {
+  sum += i;
+  console.log(`Number: ${i} | Sum: ${sum}`);
+};
+*/
+//4. Print the elements of an array
+ /*(const X = [1, 4, 2, 12];
+
+for (let i = 0; i < X.length; i++) {
+  console.log(X[i]);
+}
+*/
+//5. Find Max
+/*const x=[-4,-1,0]
+let maxNum=x[0]
+for(num of x){
+    if(num>maxNum){
+      maxNum=num
+    }
+}
+console.log(maxNum);
+*/
+//6. Get Average
+/*const X = [2,1.3];
+let sum = 0;
+for (let i = 0; i < X.length; i++) {
+  sum += X[i];
+}
+
+const average = sum / X.length;
+
+console.log(average);
+*/
+//7. Eliminate the Negatives
+/*const X = [2, -1, 4, -3];
+
+for (let i = 0; i < X.length; i++) {
+  if (X[i] < 0) {
+    X[i] = 0;
+  }
+}
+
+console.log(X);
+*/
+//8. Number to String
+/*let x=[1,4,6,-7,-1,9,8,0,-54]
+for(let i=0; i<=x.length; i++){
+    if(x[i]<0){
+     x[i]= 'turing'
+    }
+}
+console.log(x);
+*/
